cf4417eff4926026bc431e7bc0271696c4f90cf4
zuizihad <ahmedxihad@gmail.com>
2020-05-05 13:17:27 +0600

    Updates

---
+++ precache-manifest.ddd4ca381b9bcf59087532658ad0d458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3561c18dab01cf699181f8b3a7f4d9",
+    "url": "/bdCoronaTracker/index.html"
+  },
+  {
+    "revision": "e1ce04aaba5627fc05c2",
+    "url": "/bdCoronaTracker/static/css/main.c581d7f8.chunk.css"
+  },
+  {
+    "revision": "a72835732da7ff3bc7fb",
+    "url": "/bdCoronaTracker/static/js/2.09b33abd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/bdCoronaTracker/static/js/2.09b33abd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ce04aaba5627fc05c2",
+    "url": "/bdCoronaTracker/static/js/main.9f3bc729.chunk.js"
+  },
+  {
+    "revision": "a5de4383fa3a16f9fb7c",
+    "url": "/bdCoronaTracker/static/js/runtime-main.b79f086e.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/bdCoronaTracker/static/media/image.d7265326.png"
+  }
+]);
